596259f723425896e721a2c67032ed40dfcd2878
bramtechs <bramtech@telenet.be>
2024-04-20 22:38:20 +0200

    overlay a pkg

--- default.nix
+++ default.nix
@@ -1,3 +1,16 @@
 let
     sources = import ./nix/sources.nix;
-in sources.nixpkgs
+
+    hello = pkgs.writeShellScriptBin "hello" ''
+      echo "Hello from the Nix channel overlay!"
+    '';
+
+    pkgs = import sources.nixpkgs {
+      overlays = [
+        (self: super: {
+          inherit hello;
+        })
+      ];
+    };
+
+in pkgs

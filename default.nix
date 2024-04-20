74b6878ee78135e37d59d3f3063f5d33d48f9eb4
bramtechs <bramtech@telenet.be>
2024-04-20 23:22:45 +0200

    test

--- default.nix
+++ default.nix
@@ -1,13 +1,10 @@
-{ sources ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {} }:
 
 let
-  pkgs = sources.pkgs;
-
-  hello = pkgs.writeShellScriptBin "hello" ''
-    echo "Hello from the Nix channel overlay!"
-  '';
-
-  overlay = self: super: {
-    inherit hello;
-  };
-in pkgs
+    personal = {
+      hello = pkgs.writeShellScriptBin "hello" ''
+        echo "Hello from my custom Nix channel!"
+      '';
+    };
+in
+personal

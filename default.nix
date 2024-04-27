6ecb6557d1764f3d9d557ad82ce0bf68e03c39b8
bramtechs <bramtech@telenet.be>
2024-04-27 23:26:07 +0200

    add smalldeflate-cli tool

--- default.nix
+++ default.nix
@@ -5,8 +5,12 @@ let
       hello = pkgs.writeShellScriptBin "hello" ''
         echo "Hello from my custom Nix channel!"
       '';
-
+      
       raylib = pkgs.callPackage ./pkgs/raylib { };
+
+      smalldeflate-cli = pkgs.callPackage (import (builtins.fetchGit {
+        url = "https://github.com/bramtechs/smalldeflate-cli.git";
+      })) {};
     };
 in
 personal

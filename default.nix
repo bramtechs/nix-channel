d9fb27d8a107f4af692cc99f7150d633d2ffe6b7
bramtechs <bramtech@telenet.be>
2024-08-05 20:17:15 +0200

    pin pkgs

--- default.nix
+++ default.nix
@@ -1,20 +1,24 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ }:
 
 let
-    personal = {
-      lunasvg = pkgs.callPackage ./pkgs/cpp/lunasvg.nix { };
-      inicpp = pkgs.callPackage ./pkgs/cpp/inicpp.nix { };
-      inifile-cpp = pkgs.callPackage ./pkgs/cpp/inifile-cpp.nix { };
-      spdlog = pkgs.callPackage ./pkgs/cpp/spdlog.nix { };
-      
-      touch-scroll-physics-c = pkgs.callPackage (import (builtins.fetchGit {
-        url = "https://github.com/bramtechs/touch-scroll-physics-c.git";
-      })) {};
+  pkgs = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz";
+    sha256 = "sha256:0r9a5p748wj5lkpipy6r03d0lqzyv56krcf26l1367xg4nafn95c";
+  }) { };
+  personal = {
+    lunasvg = pkgs.callPackage ./pkgs/cpp/lunasvg.nix { };
+    inicpp = pkgs.callPackage ./pkgs/cpp/inicpp.nix { };
+    inifile-cpp = pkgs.callPackage ./pkgs/cpp/inifile-cpp.nix { };
+    spdlog = pkgs.callPackage ./pkgs/cpp/spdlog.nix { };
 
-      smalldeflate-cli = pkgs.callPackage (import (builtins.fetchGit {
-        url = "https://github.com/bramtechs/smalldeflate-cli.git";
-      })) {};
+    touch-scroll-physics-c = pkgs.callPackage (import (
+      builtins.fetchGit { url = "https://github.com/bramtechs/touch-scroll-physics-c.git"; }
+    )) { };
 
-    };
+    smalldeflate-cli = pkgs.callPackage (import (
+      builtins.fetchGit { url = "https://github.com/bramtechs/smalldeflate-cli.git"; }
+    )) { };
+
+  };
 in
 personal
